3ce0327dbf839cdd0e4a0cf59f131e45fcbe46b9
Salih Gundogdu <salihgundogdu@Salihs-MacBook-Pro.local>
2018-10-21 15:39:18 -0400

    feature

--- classify.py
+++ classify.py
@@ -4,17 +4,18 @@ import infer
 
 def main():
 
-    run_type = sys.argv[0]
+    run_type = sys.argv[1]
+    print('*'*120)
     print(run_type)
 
     if run_type=='train':
         os.system('python train.py')
 
     elif run_type=='test':
-        infer.infer(sys.argv[1])
+        infer.infer(sys.argv[2])
 
     elif run_type=='vis_get':
-    	infer.get_activations(sys.argv[1])
+    	infer.get_activations(sys.argv[2])
 
     else:
     	print("To run this script please enter either: 'train' or 'test <x>.png'")
