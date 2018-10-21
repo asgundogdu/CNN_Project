ec04dcf508d027ddc04a04f7ebcf0160d851d32d
Salih Gundogdu <salihgundogdu@Salihs-MacBook-Pro.local>
2018-10-21 17:51:58 -0400

    last_edits

--- train.py
+++ train.py
@@ -155,46 +155,46 @@ def train(epoch):
     print("-"*120)
 
 
-def freeze_graph(model_dir, output_node_names):
-	# if not tf.gfile.Exists(model_dir):
- #        raise AssertionError
+# def freeze_graph(model_dir, output_node_names):
+# 	# if not tf.gfile.Exists(model_dir):
+#  #        raise AssertionError
 
-    if not output_node_names:
-        print("You need to supply the name of a node to --output_node_names.")
-        return -1
+#     if not output_node_names:
+#         print("You need to supply the name of a node to --output_node_names.")
+#         return -1
 
-    # We retrieve our checkpoint fullpath
-    checkpoint = tf.train.get_checkpoint_state(model_dir)
-    input_checkpoint = checkpoint.model_checkpoint_path
+#     # We retrieve our checkpoint fullpath
+#     checkpoint = tf.train.get_checkpoint_state(model_dir)
+#     input_checkpoint = checkpoint.model_checkpoint_path
 
-     # We precise the file fullname of our freezed graph
-    absolute_model_dir = "/".join(input_checkpoint.split('/')[:-1])
-    output_graph = absolute_model_dir + "/frozen_model.pb"
+#      # We precise the file fullname of our freezed graph
+#     absolute_model_dir = "/".join(input_checkpoint.split('/')[:-1])
+#     output_graph = absolute_model_dir + "/frozen_model.pb"
 
-    # We clear devices to allow TensorFlow to control on which device it will load operations
-    clear_devices = True
+#     # We clear devices to allow TensorFlow to control on which device it will load operations
+#     clear_devices = True
 
-    # We start a session using a temporary fresh Graph
-    with tf.Session(graph=tf.Graph()) as sess:
-        # We import the meta graph in the current default Graph
-        saver = tf.train.import_meta_graph(input_checkpoint + '.meta', clear_devices=clear_devices)
+#     # We start a session using a temporary fresh Graph
+#     with tf.Session(graph=tf.Graph()) as sess:
+#         # We import the meta graph in the current default Graph
+#         saver = tf.train.import_meta_graph(input_checkpoint + '.meta', clear_devices=clear_devices)
 
-        # We restore the weights
-        saver.restore(sess, input_checkpoint)
+#         # We restore the weights
+#         saver.restore(sess, input_checkpoint)
 
-        # We use a built-in TF helper to export variables to constants
-        output_graph_def = tf.graph_util.convert_variables_to_constants(
-            sess, # The session is used to retrieve the weights
-            tf.get_default_graph().as_graph_def(), # The graph_def is used to retrieve the nodes 
-            output_node_names.split(",") # The output node names are used to select the usefull nodes
-        ) 
+#         # We use a built-in TF helper to export variables to constants
+#         output_graph_def = tf.graph_util.convert_variables_to_constants(
+#             sess, # The session is used to retrieve the weights
+#             tf.get_default_graph().as_graph_def(), # The graph_def is used to retrieve the nodes 
+#             output_node_names.split(",") # The output node names are used to select the usefull nodes
+#         ) 
 
-        # Finally we serialize and dump the output graph to the filesystem
-        with tf.gfile.GFile(output_graph, "wb") as f:
-            f.write(output_graph_def.SerializeToString())
-        print("%d ops in the final graph." % len(output_graph_def.node))
+#         # Finally we serialize and dump the output graph to the filesystem
+#         with tf.gfile.GFile(output_graph, "wb") as f:
+#             f.write(output_graph_def.SerializeToString())
+#         print("%d ops in the final graph." % len(output_graph_def.node))
 
-    return output_graph_def
+#     return output_graph_def
 
 
 def run_train():
@@ -216,7 +216,7 @@ def run_train():
 
     # [print(n.name) for n in tf.get_default_graph().as_graph_def().node]
 
-    freeze_graph("./model/trial4/", "ArgMax_1")
+    # freeze_graph("./model/trial4/", "ArgMax_1")
 
 
 # if __name__ == "__main__":
