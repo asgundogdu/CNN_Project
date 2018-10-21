a32e07ac34a27b65ecb6ac16391460cf5a5953a6
Salih Gundogdu <salihgundogdu@Salihs-MacBook-Pro.local>
2018-10-21 04:07:30 -0400

    single infer.pf

--- infer.py
+++ infer.py
@@ -4,6 +4,11 @@ import tensorflow as tf
 from helper.data import get_data_set
 from helper.model import model
 
+from PIL import Image
+# import numpy as np
+import scipy
+from scipy import misc
+
 
 test_x, test_y = get_data_set("test")
 x, y, output, y_pred_cls, global_step, learning_rate = model()
@@ -15,6 +20,8 @@ save_dir = 'model/trial4/'
 saver = tf.train.Saver()
 sess = tf.Session()
 
+class_names = ['airplane','automobile','bird','cat','deer',
+               'dog','frog','horse','ship','truck']
 
 try:
     print("\nTrying to restore last checkpoint ...")
@@ -27,9 +34,16 @@ except ValueError:
 
 
 def main():
+	image = scipy.misc.imread('cifar/test/0_cat.png')
+    image = image.astype(float)
+    image = np.array(image, dtype=float) / 255.0
+    image = image.reshape([-1, 3, 32, 32])
+    image = image.transpose([0, 2, 3, 1])
+    image = image.reshape(-1, 32*32*3)
+
     i = 0
-    predicted_class = np.zeros(shape=len(test_x[0]), dtype=np.int)
-    sess.run(y_pred_cls, feed_dict={x: test_x})
+    # predicted_class = np.zeros(shape=len(test_x[0]), dtype=np.int)
+    result = sess.run(y_pred_cls, feed_dict={x: image})
     # while i < len(test_x):
     #     j = min(i + _BATCH_SIZE, len(test_x))
     #     batch_xs = test_x[i:j, :]
@@ -40,9 +54,9 @@ def main():
     # correct = (np.argmax(test_y, axis=1) == predicted_class)
     # acc = correct.mean() * 100
     # correct_numbers = correct.sum()
-    print(test_y[0])
+    print(result[0])
     # print("Accuracy on Test-Set: {0:.2f}% ({1} / {2})".format(acc, correct_numbers, len(test_x)))
-    print(predicted_class)
+    print('Cat -- should be 3!')
 
 if __name__ == "__main__":
     main()
